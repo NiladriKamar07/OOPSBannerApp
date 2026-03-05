066cf4db5b1a5b5145ebe074ee35b8a85d36cc58
NiladriKamar07 <niladrikamar.2007@gmail.com>
2026-03-05 15:05:40 +0530

    UC5 completed

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -2,18 +2,18 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        
+        // Declare and initialize array in single statement
         String[] banner = {
-
             String.join("   ", "OOOO", "OOOO", "PPPP", "SSSS"),
             String.join("   ", "O  O", "O  O", "P   P", "S"),
             String.join("   ", "O  O", "O  O", "PPPP", "SSSS"),
             String.join("   ", "O  O", "O  O", "P", "S"),
-            String.join("   ", "OOOO", "OOOO", "P", "SSSS")
-
+            String.join("   ", "OOOO", "OOOO", "P", "SSSS"),
+            String.join("   ", "O     O", "O     O", "P", "S"),
+            String.join("   ", "OOOO   ", "OOOO   ", "P", "SSSS")
         };
 
-       
+        // Enhanced for-loop to print
         for (String line : banner) {
             System.out.println(line);
         }
