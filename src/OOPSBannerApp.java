413264a30bbda7aca6585291bbb31581d2d22c32
NiladriKamar07 <niladrikamar.2007@gmail.com>
2026-03-05 14:31:03 +0530

    Update OOPSBannerApp.java

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -3,7 +3,7 @@
 
 public class OOPSBannerApp {
  public static void main(String[] args){
-   System.out.println("   **   " + "   **   " + "******"  + "    git******");
+   System.out.println("   **   " + "   **   " + "******"  + "    ******");
    System.out.println("**    **" + "**    **" + "**   **" + "   ** ");
    System.out.println("**    **" + "**    **" + "**   **" + " **");
    System.out.println("**    **" + "**    **" + "******"  + "    ***");
