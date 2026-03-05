35daed9a43231ee43ad77ad875b8b4464b75f5a0
NiladriKamar07 <niladrikamar.2007@gmail.com>
2026-03-05 14:39:18 +0530

    feature/UC3

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,15 +1,41 @@
-
-
-
 public class OOPSBannerApp {
- public static void main(String[] args){
-   System.out.println("   **   " + "   **   " + "******"  + "    git******");
-   System.out.println("**    **" + "**    **" + "**   **" + "   ** ");
-   System.out.println("**    **" + "**    **" + "**   **" + " **");
-   System.out.println("**    **" + "**    **" + "******"  + "    ***");
-   System.out.println("**    **" + "**    **" + "**"      + "       **");
-   System.out.println("**    **" + "**    **" + "**"      + "     **");
-   System.out.println("   **  "  + "   **  "  + "**"      + "  ***");
-   
- }
+
+    public static void main(String[] args) {
+
+        String line1 = String.join("   ",
+                "OOOO",
+                "OOOO",
+                "PPPP",
+                "SSSS");
+
+        String line2 = String.join("   ",
+                "O  O",
+                "O  O",
+                "P   P",
+                "S");
+
+        String line3 = String.join("   ",
+                "O  O",
+                "O  O",
+                "PPPP",
+                "SSSS");
+
+        String line4 = String.join("   ",
+                "O  O",
+                "O  O",
+                "P",
+                "S");
+
+        String line5 = String.join("   ",
+                "OOOO",
+                "OOOO",
+                "P",
+                "SSSS");
+
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+        System.out.println(line4);
+        System.out.println(line5);
+    }
 }
